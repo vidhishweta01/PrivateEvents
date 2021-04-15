602414419d48e38c37620049c459df614ac9efca
vidhishweta01 <vidhishweta01@gmail.com>
2021-04-15 21:38:01 +0530

    added views for user, events created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :events
+  get '/user/:id', to: 'users#profile', as: :user
+  resources :users, only: :index
+  root 'events#index'
+  resources :events do
+    member do
+      get 'invite'
+      get 'cancel_invite'
+    end
+  end
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
